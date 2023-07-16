acd96a34ea01030e1da205ea233ff3dcf31b73df
8309h <harshalwagh201718@gmail.com>
2023-07-16 22:43:57 +0530

    css

--- main.js
+++ main.js
@@ -22,45 +22,74 @@ function closeMenu() {
 
 // Event Listeners: Handling toggle event
 
-const toggleSwitch = document.querySelector(
-  '.theme-switch input[type="checkbox"]'
-);
+// const toggleSwitch = document.querySelector(
+//   '.theme-switch input[type="checkbox"]'
+// );
+
+// function switchTheme(e) {
+//   if (e.target.checked) {
+//     document.documentElement.setAttribute("data-theme", "dark");
+//   } else {
+//     document.documentElement.setAttribute("data-theme", "light");
+//   }
+// }
+
+// toggleSwitch.addEventListener("change", switchTheme, false);
+
+// //  Store color theme for future visits
+
+// function switchTheme(e) {
+//   if (e.target.checked) {
+//     document.documentElement.setAttribute("data-theme", "dark");
+//     localStorage.setItem("theme", "dark"); //add this
+//   } else {
+//     document.documentElement.setAttribute("data-theme", "light");
+//     localStorage.setItem("theme", "light"); //add this
+//   }
+// }
+
+// // Save user preference on load
+
+// const currentTheme = localStorage.getItem("theme")
+//   ? localStorage.getItem("theme")
+//   : null;
+
+// if (currentTheme) {
+//   document.documentElement.setAttribute("data-theme", currentTheme);
+
+//   if (currentTheme === "dark") {
+//     toggleSwitch.checked = true;
+//   }
+// }
+
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 
 function switchTheme(e) {
   if (e.target.checked) {
-    document.documentElement.setAttribute("data-theme", "dark");
-  } else {
     document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "light");
+  } else {
+    document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark");
   }
 }
 
 toggleSwitch.addEventListener("change", switchTheme, false);
 
-//  Store color theme for future visits
-
-function switchTheme(e) {
-  if (e.target.checked) {
-    document.documentElement.setAttribute("data-theme", "dark");
-    localStorage.setItem("theme", "dark"); //add this
-  } else {
-    document.documentElement.setAttribute("data-theme", "light");
-    localStorage.setItem("theme", "light"); //add this
-  }
-}
-
-// Save user preference on load
-
-const currentTheme = localStorage.getItem("theme")
-  ? localStorage.getItem("theme")
-  : null;
-
+// Set user preference on load
+const currentTheme = localStorage.getItem("theme");
 if (currentTheme) {
   document.documentElement.setAttribute("data-theme", currentTheme);
-
-  if (currentTheme === "dark") {
+  if (currentTheme === "light") {
+    toggleSwitch.checked = true;
+  }
+} else {
+  const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  if (prefersDarkMode) {
     toggleSwitch.checked = true;
   }
 }
+
 
 //// *=============================== resume open in new tab ==================================
 
